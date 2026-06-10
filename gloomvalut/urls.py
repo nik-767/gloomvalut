e5c911ead0f7943e9243dc26e616bbd9861d7bf0
nikhil <nikhilkalra124421@gmail.com>
2026-06-10 16:25:12 +0530

    Wire up registration and simplejwt token authentication URLs

--- gloomvalut/urls.py
+++ gloomvalut/urls.py
@@ -18,11 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import *
+from core.views import * 
 from rest_framework import routers
 from core import views
 from django.urls import path, include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
 
 router = routers.DefaultRouter()
 router.register(r'gloomvalutview', views.gloomvalutview)
@@ -35,6 +39,10 @@ urlpatterns = [
     path('home/', home , name='home' ),
     path('review/<int:Destination_id>/', review_view, name='review_view'),
     path('review/<int:id>/update.html', Update_view, name='Update_view'),
+    path('api/register/',Register_api.as_view(), name='auth_register'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('api/', include(router.urls)),
 
 
