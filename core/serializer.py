1a52c327965d6426ae18593302468b8a35ab002e
nikhil <nikhilkalra124421@gmail.com>
2026-06-10 16:21:24 +0530

    Create Registerseralizer and loginseralizer in core app

--- core/serializer.py
+++ core/serializer.py
@@ -1,7 +1,29 @@
 from rest_framework import serializers
 from .models import Destination
+from django.contrib.auth.models import User
 
 class gloomvalutseralizer(serializers.ModelSerializer):
     class Meta:
         model = Destination
-        fields = '__all__'
+        fields = "__all__"
+
+class Registerseralizer(serializers.ModelSerializer):
+    email = serializers.EmailField(required=True)
+    class Meta:
+        model = User
+        fields =  ('id', 'username','email','password')
+        extra_kwargs = {      # iska kamm hai exta rules and properties  likhna bina pura fields k pura seralizer likha
+        'password' : {'write_only':True}
+        }
+
+    def create(self,validated_data):
+    # hash the plan text pass automatically 
+        return User.objects.create_user(
+        username= validated_data['username'],
+        password= validated_data['password'],
+        email= validated_data['email']
+    )
+
+class loginseralizer(serializers.Serializer):
+    username = serializers.CharField(required = True)
+    password = serializers.CharField(required = True, write_only= True)
