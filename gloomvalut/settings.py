05d43051b8e6c5dfa50f99f90da3726b15e663f2
nikhil <nikhilkalra124421@gmail.com>
2026-06-22 13:39:56 +0530

    transerfing sqlite db to postgres athrough using json . and dump data

--- gloomvalut/settings.py
+++ gloomvalut/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gloomvalut.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gloomvalut',            # Database name from your image
+        'USER': 'postgres',              # The default pgAdmin superuser
+        'PASSWORD': 'nikki', # The password you set when installing pgAdmin
+        'HOST': '127.0.0.1',             # Tells Django pgAdmin is running on your local machine
+        'PORT': '5432',                  # Default PostgreSQL port
     }
 }
 
