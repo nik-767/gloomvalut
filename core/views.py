2672fc4c1d44e6db7e1d172ef876a8eee405863f
nikhil <nikhilkalra124421@gmail.com>
2026-06-26 20:20:49 +0530

    adding changes

--- core/views.py
+++ core/views.py
@@ -14,7 +14,6 @@ from django.db.models import Avg
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import PermissionDenied
-from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 
 
