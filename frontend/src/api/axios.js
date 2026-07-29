b5dc3c8b2277b789c160792756d4d8eb30b672f6
Nikhil Kalra <nikhilkalra124421@gmail.com>
2026-07-29 14:42:55 +0530

    adding interceptors to axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -4,4 +4,19 @@ const axiosInstance = axios.create({
     baseURL: 'https://127.0.0.1:8000/api/', // Replace with your Django backend URL
 });
 
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("access_token");
+
+        if(token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
 export default axiosInstance;
