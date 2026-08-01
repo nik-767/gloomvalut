bf23d5768afdf8dbcc6d65d9cded49c8e7d269e2
Nikhil Kalra <nikhilkalra124421@gmail.com>
2026-08-01 20:48:04 +0530

    refactor(app): wrap app root with AuthProvider and AppDataProvider context tree

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
-import AppRoutes from "./routes/AppRoutes";
-import { AuthProvider } from "./context/AuthContext";
+import AppRoutes from './routes/AppRoutes';
+import { AuthProvider } from './context/AuthContext';
+import { AppDataProvider } from './context/AppDataContext';
 
-import "./index.css";
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <AppRoutes />
+        <AppDataProvider>
+          <AppRoutes />
+        </AppDataProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
